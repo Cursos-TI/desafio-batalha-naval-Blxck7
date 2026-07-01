320ba203921f46c18a6e90fbbe37d72f4b9c9af5
Blxck7 <guilhermelopessouza2017@gmail.com>
2026-07-01 16:35:43 -0300

    NÍVEL MESTRE CONCLUÍDO!!!

--- batalhanaval.c
+++ batalhanaval.c
@@ -1,66 +1,138 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-    // 1 - Variáveis
-    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',};
-
-    // 2 - Tabuleiro
-    int tabuleiro[10][10];
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++){
-            tabuleiro[i][j] = 0;
-        }
-    }
-
-    // 3 - Navio na Horizontal
-    for(int i = 0; i < 3; i++){
-        tabuleiro[2][3 + i] = 3;
-    }
-
-    // 4 - Navio na Vertical
-    for(int i = 0; i < 3; i++){
-        tabuleiro[5 + i][7] = 3;
-    }
-
-    // 5 - Navio na Diagonal (Esquerda para direita)
-    for(int i = 0; i < 3; i++){
-        tabuleiro[4 + i][1 + i] = 3;
-    }
-
-    // 6 - Navio na Diagonal (Direita para esquerda)
-    for(int i = 0; i < 3; i++){
-        tabuleiro[9 - i][3 + i] = 3;
-    }
-
-    // 7 -  Mostrando o Tabuleiro
+void exibirTabuleiro(char letras[], int tabuleiro[10][10]) {
+    // Mostra o tabuleiro
     printf("TABULEIRO BATALHA NAVAL\n");
     printf("  ");
 
-    // 8 - Mostra as letras da Coordenada
-    for(int i = 0; i < 10; i++){
-        printf(" %c", linha[i]);
+    // Letras da coordenada
+    for(int i = 0; i < 10; i++) {
+        printf(" %c", letras[i]);
     }
     printf("\n");
 
-    // 9 - Mostra os números da Coordenada
-    for(int i = 0; i < 10; i++){
+    // Números da coordenada
+    for (int i = 0; i < 10; i++) {
 
-    // 10 - Ajustando o 10 para ele não ficar fora de linha no tabuleiro
-        if(i == 9){
+        // Ajuste para o 10 não ficar fora da linha do tabuleiro
+        if (i == 9) {
             printf("%d", i + 1);
         } else {
             printf(" %d", i + 1);
         }
 
-    // 11 - Exibe o tabuleiro
-    for (int j = 0; j < 10; j++){
-        printf(" %d", tabuleiro[i][j]);
+        // Mostra o tabuleiro 10x10
+        for (int j = 0; j < 10; j++) {
+            printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
     }
+}
 
+int main() {
+    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int escolhaJogador, linha, coluna;
+    char colunaLetra;
 
+    // 1 - Tabuleiro
+    int tabuleiro[10][10];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    // 2 - Návio na horizontal
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[2][3 + i] = 3;
+    }
+
+    // 3 - Návio na vertical
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[5 + i][7] = 3;
+    }
+
+    // 4 Návio na diagonal (Esquerda para Direita)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[4 + i][1 + i] = 3;
+    }
+
+    // 5 - Návio na diagonal (Direita para Esquerda)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[9 - i][3 + i] = 3;
+    }
+
+    while (1) {
+        system("cls");
+        exibirTabuleiro(letras, tabuleiro);
+
+        printf("\n--- Habilidades ---\n");
+        printf("1 - Cone\n");
+        printf("2 - Cruz\n");
+        printf("3 - Octaedro\n");
+        printf("Escolha uma habilidade: ");
+        scanf("%d", &escolhaJogador);
+
+        // Escolhendo as coordenadas
+        printf("Escolha a linha: ");
+        scanf(" %d", &linha);
+        linha = linha - 1;
+        printf("Escolha a coluna: ");
+        scanf(" %c", &colunaLetra);
+
+        for (int i = 0; i < 10; i++) {
+            if (letras[i] == colunaLetra) {
+                coluna = i;
+            }
+        }
+
+        switch (escolhaJogador) {
+            case 1:
+                // Cone
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        if (i == 0) {
+                            tabuleiro[linha][coluna] = 5;
+                        } else if (i == 1 && j < 3) {
+                            tabuleiro[linha + i][coluna - i + j] = 5;
+                        } else if (i == 2) {
+                            tabuleiro[linha + i][coluna - i + j] = 5;
+                        }
+                    }
+                }
+            break;
+            case 2:
+                // Cruz
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        if (i != 2) {
+                            tabuleiro[linha + i][coluna] = 5;
+                        } else {
+                            tabuleiro[linha + i][coluna - i + j] = 5;
+                        }
+                    }
+                }
+            break;
+            case 3:
+                // Octaedro
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        if (i == 0 || i == 4) {
+                            tabuleiro[linha + i][coluna] = 5;
+                        } else if ((i == 1 || i == 3) && j < 3) {
+                            tabuleiro[linha + i][coluna - 1 + j] = 5;
+                        } else if (i == 2) {
+                            tabuleiro[linha + i][coluna - 2 + j] = 5;
+                        }
+                    }
+                }
+            break;
+            default:
+                printf("Opção Invalida!");
+            break;
+        }
+    }
 
     return 0;
 }
